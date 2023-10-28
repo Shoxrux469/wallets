fcb4c7f7aec2855e12ab6b63c3c3909f8413e8d6
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2023-10-28 12:27:45 +0500

    some alterations

--- registr/registr.js
+++ registr/registr.js
@@ -18,29 +18,73 @@ form.onsubmit = (e) => {
     users[key] = value;
   });
 
-  fetch(base_url + "/users", {
-    method: "post",
-    body: JSON.stringify(users),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => {
-    if (res.status === 200 || res.status === 201) {
-      let error = false;
+  axios.get(base_url + '/users?email=' + users.email)
+    .then(res => {
+      if (res.status !== 200 && res.status !== 201) {
+        return
+      } else {
+        let error = false;
+  
+        inps.forEach((inp) => {
+          if (inp.value.length === 0) {
+            inp.classList.add("error");
+            error = true;
+          } else {
+            inp.classList.remove("error");
+          }
+        });
+        
 
-      inps.forEach((inp) => {
-        if (inp.value.length === 0) {
-          inp.classList.add("error");
-          error = true;
+        if (error === false) {
+          if (res.data.length > 0) {
+            console.log('User already exists');
+            console.log(res);
+            return
+          } else {
+            document.location.href = `./index.html`;
+            form.reset();
+          }
         } else {
-          inp.classList.remove("error");
+            return
         }
-      });
-
-      if (error === false) {
-        document.location.href = `./index.html`;
-        form.reset();
       }
-    }
-  });
+
+      axios.post(base_url + '/users', users)
+        .then(res => console.log(res))
+    })
+
+  // fetch(base_url + '/users?email=' + users.email)
+  //   .then(res => {
+  //     if(res.status !== 200 && res.status !== 201) return 
+  //     if(length.data.length > 0) {
+  //       alert('User already exists')
+  //       return
+  //     }
+  //   })
+    
+  // fetch(base_url + "/users", {
+  //   method: "post",
+  //   body: JSON.stringify(users),
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // }).then((res) => {
+    // if (res.status === 200 || res.status === 201) {
+    //   let error = false;
+
+    //   inps.forEach((inp) => {
+    //     if (inp.value.length === 0) {
+    //       inp.classList.add("error");
+    //       error = true;
+    //     } else {
+    //       inp.classList.remove("error");
+    //     }
+    //   });
+
+    //   if (error === false) {
+    //     document.location.href = `./index.html`;
+    //     form.reset();
+    //   }
+    // }
+  // });
 };
